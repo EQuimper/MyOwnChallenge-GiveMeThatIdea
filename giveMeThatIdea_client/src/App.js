3fe1e954aa7f8dfb40ea1aa73f91e717d997bdbd
EQuimper <quimperemanuel@gmail.com>
2016-12-17 00:43:17 -0500

    Add the jwt token for the axios

--- giveMeThatIdea_client/src/App.js
+++ giveMeThatIdea_client/src/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import { Grid } from 'semantic-ui-react';
 import { NavBar, SideMenu } from './modules';
+import { store } from './redux/store';
 
 /**
 * AXIOS DEFAULTS SETUP
@@ -10,28 +11,33 @@ import { NavBar, SideMenu } from './modules';
 axios.defaults.baseURL = 'http://localhost:3001/api/v1';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const App = ({ children, location, user }) => (
-  <div>
-    <NavBar path={location.pathname} auth={user} />
-    {console.log({ user })}
-    {user ? (
-      <Grid columns={2}>
-        <Grid.Row>
-          <Grid.Column width={1}>
-            <SideMenu />
-          </Grid.Column>
-          <Grid.Column width={15}>
-            {children}
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    ) : (
-      <div>
-        {children}
-      </div>
-    )}
-  </div>
-);
+const App = ({ children, location, user }) => {
+  if (store.getState().auth.user !== null) {
+    axios.defaults.headers.common['Authorization'] = store.getState().auth.token;
+  }
+  return (
+    <div>
+      <NavBar path={location.pathname} auth={user} />
+      {console.log({ user })}
+      {user ? (
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column width={1}>
+              <SideMenu />
+            </Grid.Column>
+            <Grid.Column width={15}>
+              {children}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      ) : (
+        <div>
+          {children}
+        </div>
+      )}
+    </div>
+  )
+};
 
 export default connect(
   state => ({
