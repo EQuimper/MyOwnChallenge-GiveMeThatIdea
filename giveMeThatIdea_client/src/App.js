491414f886271f5f20b57091be4113b9ca170243
EQuimper <quimperemanuel@gmail.com>
2016-12-16 21:56:42 -0500

    Connect for see user

--- giveMeThatIdea_client/src/App.js
+++ giveMeThatIdea_client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import { Grid } from 'semantic-ui-react';
 import { NavBar, SideMenu } from './modules';
@@ -9,20 +10,31 @@ import { NavBar, SideMenu } from './modules';
 axios.defaults.baseURL = 'http://localhost:3001/api/v1';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const App = ({ children, location }) => (
+const App = ({ children, location, user }) => (
   <div>
-    <NavBar path={location.pathname} />
-    <Grid columns={2}>
-      <Grid.Row>
-        <Grid.Column width={1}>
-          <SideMenu />
-        </Grid.Column>
-        <Grid.Column width={15}>
-          {children}
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <NavBar path={location.pathname} auth={user} />
+    {console.log({ user })}
+    {user ? (
+      <Grid columns={2}>
+        <Grid.Row>
+          <Grid.Column width={1}>
+            <SideMenu />
+          </Grid.Column>
+          <Grid.Column width={15}>
+            {children}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    ) : (
+      <div>
+        {children}
+      </div>
+    )}
   </div>
 );
 
-export default App;
+export default connect(
+  state => ({
+    user: state.auth.user
+  })
+)(App);
