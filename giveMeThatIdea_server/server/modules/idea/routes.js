6a42b4875d58a44a2753ff6c1572bfdb51246d88
EQuimper <quimperemanuel@gmail.com>
2016-12-17 02:51:50 -0500

    add put and get

--- giveMeThatIdea_server/server/modules/idea/routes.js
+++ giveMeThatIdea_server/server/modules/idea/routes.js
@@ -9,5 +9,7 @@ const ideaRoutes = new Router();
 
 ideaRoutes.route('/ideas/new').post(requireAuth, IdeaController.createIdea);
 ideaRoutes.route('/ideas/:id').delete(requireAuth, IdeaController.deleteIdea);
+ideaRoutes.route('/ideas/:id').put(requireAuth, IdeaController.updateIdea);
+ideaRoutes.route('/ideas/').get(requireAuth, IdeaController.getAllIdea);
 
 export default ideaRoutes;
