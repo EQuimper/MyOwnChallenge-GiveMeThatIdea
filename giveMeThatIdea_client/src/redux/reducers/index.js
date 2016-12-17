c726415b1b38f57796d0dacc67bc3f4dd09c9b3c
EQuimper <quimperemanuel@gmail.com>
2016-12-17 00:42:45 -0500

    Add react-redux-toastr

--- giveMeThatIdea_client/src/redux/reducers/index.js
+++ giveMeThatIdea_client/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import {reducer as toastrReducer} from 'react-redux-toastr';
 import { reducer as formReducer } from 'redux-form';
 import { AuthReducer } from '../../modules';
 
 export default combineReducers({
+  auth: AuthReducer,
   routing: routerReducer,
   form: formReducer,
-  auth: AuthReducer
+  toastr: toastrReducer
 });
