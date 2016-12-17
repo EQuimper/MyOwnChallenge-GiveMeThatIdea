89fbf696be0ae85970c90d63983b3a7401901fcb
EQuimper <quimperemanuel@gmail.com>
2016-12-17 00:43:49 -0500

    Create toastr

--- giveMeThatIdea_client/src/index.js
+++ giveMeThatIdea_client/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
 import { store } from './redux/store';
 import Routes from './Routes';
 import './index.css';
 
 render(
   <Provider store={store}>
-    <Routes />
+    <div>
+      <Routes />
+      <ReduxToastr
+        timeOut={4000}
+        newestOnTop={false}
+        preventDuplicates
+        position="top-right"
+        transitionIn="bounceIn"
+        transitionOut="bounceOut"
+        progressBar
+      />
+    </div>
   </Provider>,
   document.getElementById('root')
 );
