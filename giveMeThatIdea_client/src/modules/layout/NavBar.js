15e9392308443aae2d49e208b4b584efe82665a0
EQuimper <quimperemanuel@gmail.com>
2016-12-16 21:56:27 -0500

    View if user

--- giveMeThatIdea_client/src/modules/layout/NavBar.js
+++ giveMeThatIdea_client/src/modules/layout/NavBar.js
@@ -2,20 +2,28 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import { Menu, Container } from 'semantic-ui-react'
 
-const NavBar = ({ path }) => (
+const NavBar = ({ path, auth }) => (
   <Menu pointing secondary>
     <Container>
       <Menu.Item onClick={() => browserHistory.push('/')}>
         GetMeThatIdea
       </Menu.Item>
-      <Menu.Menu position="right">
-        <Menu.Item active={path === '/signup'} onClick={() => browserHistory.push('/signup')}>
-          Sign Up
-        </Menu.Item>
-        <Menu.Item active={path === '/login'} onClick={() => browserHistory.push('/login')}>
-          Login
-        </Menu.Item>
-      </Menu.Menu>
+      {!auth ? (
+        <Menu.Menu position="right">
+          <Menu.Item active={path === '/signup'} onClick={() => browserHistory.push('/signup')}>
+            Sign Up
+          </Menu.Item>
+          <Menu.Item active={path === '/login'} onClick={() => browserHistory.push('/login')}>
+            Login
+          </Menu.Item>
+        </Menu.Menu>
+      ) : (
+        <Menu.Menu position="right">
+          <Menu.Item active={path === '/help'} onClick={() => browserHistory.push('/help')}>
+            How that work?
+          </Menu.Item>
+        </Menu.Menu>
+      )}
     </Container>
   </Menu>
 );
