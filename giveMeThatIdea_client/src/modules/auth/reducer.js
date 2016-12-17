74dfc4af1fc1c77f5aab20290b4d6d4894dc9f01
EQuimper <quimperemanuel@gmail.com>
2016-12-17 00:44:16 -0500

    Finish the reducer for the auth

--- giveMeThatIdea_client/src/modules/auth/reducer.js
+++ giveMeThatIdea_client/src/modules/auth/reducer.js
@@ -1,11 +1,18 @@
-import { LOGIN_USER, LOGIN_USER_SUCCESS, LOGIN_USER_ERROR } from './actions';
+import {
+  LOGIN_USER,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  SIGNUP_USER,
+  SIGNUP_USER_ERROR,
+  SIGNUP_USER_SUCCESS
+} from './actions';
 
 const INITIAL_STATE = {
   user: null,
   token: null,
   loading: false,
   message: null,
-  error: null
+  error: false
 }
 
 export default (state = INITIAL_STATE, action) => {
@@ -15,12 +22,31 @@ export default (state = INITIAL_STATE, action) => {
         loading: true
       };
     case LOGIN_USER_SUCCESS:
-      return { ...state,
+      return {
+        message: null,
+        error: false,
         user: action.user,
         token: action.token,
         loading: false
       };
     case LOGIN_USER_ERROR:
+      return { ...state,
+        message: action.message,
+        error: true
+      };
+    case SIGNUP_USER:
+      return { ...state,
+        loading: true
+      };
+    case SIGNUP_USER_SUCCESS:
+      return {
+        message: null,
+        error: false,
+        user: action.user,
+        token: action.token,
+        loading: false
+      };
+    case SIGNUP_USER_ERROR:
       return { ...state,
         message: action.message,
         error: true
