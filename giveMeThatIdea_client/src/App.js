e2d083975b03ab2ccec2600980e0c43b6e937841
EQuimper <quimperemanuel@gmail.com>
2016-12-18 03:22:45 -0500

    Create jwt security function

--- giveMeThatIdea_client/src/App.js
+++ giveMeThatIdea_client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { Grid } from 'semantic-ui-react';
 import { NavBar, SideMenu } from './modules';
-import { store } from './redux/store';
+import { checkToken } from './modules/auth/actions';
 
 /**
 * AXIOS DEFAULTS SETUP
@@ -11,36 +11,43 @@ import { store } from './redux/store';
 axios.defaults.baseURL = 'http://localhost:3001/api/v1';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const App = ({ children, location, user }) => {
-  if (store.getState().auth.user !== null) {
-    axios.defaults.headers.common['Authorization'] = store.getState().auth.token;
+class App extends Component {
+  componentWillMount() {
+    this.props.checkToken();
   }
-  return (
-    <div>
-      <NavBar path={location.pathname} auth={user} />
-      {console.log({ user })}
-      {user ? (
-        <Grid columns={2}>
-          <Grid.Row>
-            <Grid.Column width={1}>
-              <SideMenu />
-            </Grid.Column>
-            <Grid.Column width={15}>
-              {children}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      ) : (
-        <div>
-          {children}
-        </div>
-      )}
-    </div>
-  )
-};
+  render() {
+    const { children, location, auth } = this.props;
+    const { user, token } = auth;
+    if (user) {
+      axios.defaults.headers.common['Authorization'] = token;
+    }
+    return (
+      <div>
+        <NavBar path={location.pathname} auth={user} />
+        {user ? (
+          <Grid columns={2}>
+            <Grid.Row>
+              <Grid.Column width={1}>
+                <SideMenu />
+              </Grid.Column>
+              <Grid.Column width={15}>
+                {children}
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        ) : (
+          <div>
+            {children}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default connect(
   state => ({
-    user: state.auth.user
-  })
+    auth: state.auth
+  }),
+  { checkToken }
 )(App);
