ac359e2818102402bc70dc42ab59e9becb8e3dcc
EQuimper <quimperemanuel@gmail.com>
2016-12-20 14:28:06 -0500

    Route with slug

--- giveMeThatIdea_server/server/modules/idea/routes.js
+++ giveMeThatIdea_server/server/modules/idea/routes.js
@@ -8,6 +8,7 @@ routes.route('/ideas/new').post(requireAuth, IdeaController.createIdea);
 routes.route('/ideas/:id').delete(requireAuth, IdeaController.deleteIdea);
 routes.route('/ideas/:id').put(requireAuth, IdeaController.updateIdea);
 routes.route('/ideas/').get(requireAuth, IdeaController.getAllIdea);
+routes.route('/ideas/:slug').get(requireAuth, IdeaController.getOneIdea);
 routes.route('/ideas/asyncIdeaTitle').post(requireAuth, IdeaController.asyncIdeaTitle);
 
 export default routes;
