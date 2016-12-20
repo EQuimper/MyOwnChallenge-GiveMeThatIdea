4ac0a068fb3ba92bb65798a5e15f84a8aeb45af8
EQuimper <quimperemanuel@gmail.com>
2016-12-20 03:11:47 -0500

    Add category validation

--- giveMeThatIdea_client/src/modules/idea/create/components/validation.js
+++ giveMeThatIdea_client/src/modules/idea/create/components/validation.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import toastr from 'react-redux-toastr';
+import { unauthUser } from '../../../auth/actions';
 
 export const createIdeaValidation = values => {
   const errors = {};
@@ -15,15 +17,26 @@ export const createIdeaValidation = values => {
     errors.description = 'Description need to be a bit longer';
   }
 
+  if (!values.category) {
+    errors.category = 'Category is Required';
+  }
+
   return errors;
 };
 
 export const ideaTitleAsyncValidate = values => {
   return axios.post('/ideas/asyncIdeaTitle', { title: values.title })
-    .then(res => {
-      console.log({ res });
-      if (res.data.exist) {
-        throw { title: res.data.message };
+    .then(
+      res => {
+        console.log({ res });
+        if (res.data.exist) {
+          throw { title: res.data.message };
+        }
+      },
+      err => {
+        if (err.response.status === 401) {
+          return toastr.error('Unauthorized', 'Plz log again!');
+        }
       }
-    });
+    );
 }
