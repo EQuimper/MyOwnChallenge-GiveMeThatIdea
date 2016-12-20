73344c40cf629ad43ce4d5abc5f3f13d1e417997
EQuimper <quimperemanuel@gmail.com>
2016-12-20 03:10:23 -0500

    remove auth need

--- giveMeThatIdea_server/server/modules/user/routes.js
+++ giveMeThatIdea_server/server/modules/user/routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import * as UserController from './controller';
-import { requireAuth, requireLogin, checkToken } from '../../helpers';
+import { requireLogin, checkToken } from '../../helpers';
 
 const routes = new Router();
 
 routes.route('/auth/signup').post(UserController.signup);
 routes.route('/auth/login').post(requireLogin, UserController.login);
-routes.route('/auth/asyncemail').post(requireAuth, UserController.asyncEmail);
+routes.route('/auth/asyncemail').post(UserController.asyncEmail);
 routes.route('/auth/checkToken').post(checkToken);
 routes.route('/auth/forgotPassword').post(UserController.forgotPassword);
 routes.route('/auth/resetPassword/:resetToken').post(UserController.resetPassword);
