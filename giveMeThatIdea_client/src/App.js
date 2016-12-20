5d484df3b160d9101fddeccd8aae8705999f1033
EQuimper <quimperemanuel@gmail.com>
2016-12-19 23:10:06 -0500

    Add new logout action

--- giveMeThatIdea_client/src/App.js
+++ giveMeThatIdea_client/src/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import { Grid } from 'semantic-ui-react';
 import { NavBar, SideMenu } from './modules';
-import { checkToken } from './modules/auth/actions';
+import { checkToken, logoutUser } from './modules/auth/actions';
 
 /**
 * AXIOS DEFAULTS SETUP
@@ -11,19 +11,40 @@ import { checkToken } from './modules/auth/actions';
 axios.defaults.baseURL = 'http://localhost:3001/api/v1';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
+// export const instance = axios.create({
+//   baseURL: 'http://localhost:3001/api/v1',
+// });
+
+// axios.interceptors.response.use((
+//   res => console.log({ res }),
+//   err => console.log({ err })
+// ));
+
+// axios.interceptors.response.use(
+//   undefined,
+//   // res => {
+//   //   console.log({ res });
+//   // },
+//   err => {
+//     console.log({ err });
+//   }
+// );
+
+console.log(axios.defaults.headers);
+
 class App extends Component {
   componentWillMount() {
     this.props.checkToken();
   }
   render() {
-    const { children, location, auth } = this.props;
+    const { children, location, auth, logoutUser } = this.props;
     const { user, token } = auth;
-    if (user) {
-      axios.defaults.headers.common['Authorization'] = token;
-    }
+    // if (user) {
+    //   axios.defaults.headers.common['Authorization'] = token;
+    // }
     return (
       <div>
-        <NavBar path={location.pathname} auth={user} />
+        <NavBar path={location.pathname} auth={user} logoutUser={logoutUser} />
         {user ? (
           <Grid columns={2}>
             <Grid.Row>
@@ -49,5 +70,5 @@ export default connect(
   state => ({
     auth: state.auth
   }),
-  { checkToken }
+  { checkToken, logoutUser }
 )(App);
