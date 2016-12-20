b8a017ea010e8eece2c075f6ca5b2131a285108e
EQuimper <quimperemanuel@gmail.com>
2016-12-19 23:10:41 -0500

    can logout

--- giveMeThatIdea_client/src/modules/layout/NavBar.js
+++ giveMeThatIdea_client/src/modules/layout/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import { Menu, Container } from 'semantic-ui-react'
 
-const NavBar = ({ path, auth }) => (
+const NavBar = ({ path, auth, logoutUser }) => (
   <Menu pointing secondary>
     <Container>
       <Menu.Item onClick={() => browserHistory.push('/')}>
@@ -21,6 +21,9 @@ const NavBar = ({ path, auth }) => (
         <Menu.Menu position="right">
           <Menu.Item active={path === '/help'} onClick={() => browserHistory.push('/help')}>
             How that work?
+          </Menu.Item>
+          <Menu.Item onClick={() => logoutUser()}>
+            Logout
           </Menu.Item>
         </Menu.Menu>
       )}
