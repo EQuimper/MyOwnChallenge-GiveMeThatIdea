524b1819c1146935fde4aab49e393af7e26a6952
EQuimper <quimperemanuel@gmail.com>
2016-12-20 04:26:09 -0500

    Correction of container

--- giveMeThatIdea_client/src/modules/idea/create/CreateIdeaContainer.js
+++ giveMeThatIdea_client/src/modules/idea/create/CreateIdeaContainer.js
@@ -1,8 +1,43 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CreateIdea from './CreateIdea';
+import axios from 'axios';
+import { Grid } from 'semantic-ui-react';
+import { CreateIdeaForm } from './components';
 import { createIdea } from './actions';
+
+// TODO: CHANGE LOADING FOR A SCREEN
+
+class CreateIdeaContainer extends Component {
+  state = { categories: [], isFetching: false };
+  componentWillMount () {
+    this.setState({ isFetching: true });
+    axios.get('/categories')
+      .then(
+        res => {
+          return this.setState({
+            categories: res.data.categories,
+            isFetching: false
+          });
+        },
+        err => console.log(err)
+      )
+  }
+  render() {
+    if (this.state.isFetching) {
+      return <h1>Loading...</h1>;
+    }
+    return (
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <h1>Create an Idea</h1>
+          <CreateIdeaForm categories={this.state.categories} createIdea={this.props.createIdea} />
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 
 export default connect(
   null,
   { createIdea }
-)(CreateIdea);
+)(CreateIdeaContainer);
