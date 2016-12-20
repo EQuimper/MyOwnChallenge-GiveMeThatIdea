8ed7c7d365d8b0874a43645ce3bac761caa008e6
EQuimper <quimperemanuel@gmail.com>
2016-12-20 03:11:38 -0500

    Fetch categories name

--- giveMeThatIdea_client/src/modules/idea/create/CreateIdea.js
+++ giveMeThatIdea_client/src/modules/idea/create/CreateIdea.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { Grid } from 'semantic-ui-react';
 import { CreateIdeaForm } from './components';
 
-const CreateIdea = () => (
-  <Grid centered columns={2}>
-    <Grid.Column>
-      <h1>Create an Idea</h1>
-      <CreateIdeaForm />
-    </Grid.Column>
-  </Grid>
-);
+// TODO: CHANGE LOADING FOR A SCREEN
+
+class CreateIdea extends Component {
+  state = { categories: [], isFetching: false };
+  componentWillMount () {
+    this.setState({ isFetching: true });
+    axios.get('/categories')
+      .then(
+        res => {
+          return this.setState({
+            categories: res.data.categories,
+            isFetching: false
+          });
+        },
+        err => console.log(err)
+      )
+  }
+  render() {
+    if (this.state.isFetching) {
+      return <h1>Loading...</h1>;
+    }
+    return (
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <h1>Create an Idea</h1>
+          <CreateIdeaForm categories={this.state.categories} />
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 
 export default CreateIdea;
