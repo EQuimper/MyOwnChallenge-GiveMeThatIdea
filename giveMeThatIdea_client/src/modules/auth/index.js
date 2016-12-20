18763b8a9ad1ffddb5d5c7db8b4b3da397172a59
EQuimper <quimperemanuel@gmail.com>
2016-12-19 23:11:03 -0500

    Import resetpassword

--- giveMeThatIdea_client/src/modules/auth/index.js
+++ giveMeThatIdea_client/src/modules/auth/index.js
@@ -1,5 +1,6 @@
 import LoginContainer from './login/LoginContainer';
 import SignupContainer from './signup/SignupContainer';
+import ResetPasswordContainer from './reset_password/ResetPasswordContainer';
 import AuthReducer from './reducer';
 
-export { LoginContainer, AuthReducer, SignupContainer };
+export { LoginContainer, AuthReducer, SignupContainer, ResetPasswordContainer };
