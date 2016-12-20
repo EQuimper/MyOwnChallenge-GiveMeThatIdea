bf17cc0633c30c111af40a98d98fb34c06574809
EQuimper <quimperemanuel@gmail.com>
2016-12-19 23:09:31 -0500

    own file

---
+++ giveMeThatIdea_server/server/helpers/setUserInfo.js
@@ -0,0 +1,8 @@
+/*
+* Set the user info we sent to the front-end
+* This is for security cause we don't want to share password etc...
+*/
+export const setUserInfo = user => ({
+  email: user.local.email,
+  id: user._id
+});
