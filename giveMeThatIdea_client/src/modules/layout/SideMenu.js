207b91ac7db6ffd3851d44268f95ea3e9193e68f
EQuimper <quimperemanuel@gmail.com>
2016-12-20 14:35:24 -0500

    Clean sidemenu

--- giveMeThatIdea_client/src/modules/layout/SideMenu.js
+++ giveMeThatIdea_client/src/modules/layout/SideMenu.js
@@ -5,10 +5,6 @@ import { browserHistory } from 'react-router';
 const SideMenu = () => (
   <Menu vertical>
     <Menu.Item>
-      <Input placeholder="Search..." />
-    </Menu.Item>
-
-    <Menu.Item>
       Ideas
       <Menu.Menu>
         <Menu.Item>
@@ -22,23 +18,7 @@ const SideMenu = () => (
         </Menu.Item>
       </Menu.Menu>
     </Menu.Item>
-
-        <Menu.Item>
-          <Icon name='grid layout' />
-          Browse
-        </Menu.Item>
-        <Menu.Item>
-          Messages
-        </Menu.Item>
-
-        <Dropdown as={Menu.Item} text='More'>
-          <Dropdown.Menu>
-            <Dropdown.Item icon='edit' text='Edit Profile' />
-            <Dropdown.Item icon='globe' text='Choose Language' />
-            <Dropdown.Item icon='settings' text='Account Settings' />
-          </Dropdown.Menu>
-        </Dropdown>
-      </Menu>
+  </Menu>
 );
 
 export default SideMenu;
