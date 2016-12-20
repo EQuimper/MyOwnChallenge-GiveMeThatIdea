f5b1dafb4c5ab7356c5680c4bbaec6a20f9133d3
EQuimper <quimperemanuel@gmail.com>
2016-12-20 03:09:12 -0500

    Change expiration

--- giveMeThatIdea_server/server/helpers/generateToken.js
+++ giveMeThatIdea_server/server/helpers/generateToken.js
@@ -7,5 +7,5 @@ import serverConfig from '../config/serverConfig';
 export const generateToken = user => jwt.sign({
   sub: user.id },
   serverConfig.JWT_SECRET,
-  { expiresIn: '10s' }
+  { expiresIn: '1h' }
 );
