18be8971249830cd0ae3b1d62731235cfb9c813b
EQuimper <quimperemanuel@gmail.com>
2016-12-21 03:51:41 -0500

    take back async email

--- giveMeThatIdea_server/server/modules/user/routes.js
+++ giveMeThatIdea_server/server/modules/user/routes.js
@@ -7,6 +7,7 @@ const routes = new Router();
 routes.route('/auth/signup').post(UserController.signup);
 routes.route('/auth/login').post(requireLogin, UserController.login);
 routes.route('/auth/asyncUsername').post(UserController.asyncUsername);
+routes.route('/auth/asyncEmail').post(UserController.asyncEmail);
 routes.route('/auth/checkToken').post(checkToken);
 routes.route('/auth/forgotPassword').post(UserController.forgotPassword);
 routes.route('/auth/resetPassword/:resetToken').post(UserController.resetPassword);
