2210d62eb3372469c466f76f7440b63ade1bd474
EQuimper <quimperemanuel@gmail.com>
2016-12-20 21:47:59 -0500

    Make idea page

--- giveMeThatIdea_client/src/modules/idea/single/IdeaPage.js
+++ giveMeThatIdea_client/src/modules/idea/single/IdeaPage.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button, Icon, Image, Item, Label, Divider, Grid, Comment, Form, Header } from 'semantic-ui-react';
+import { CommentsGroup } from './components';
 
 const IdeaPage = ({ idea }) => (
-  <div>
-    <h1>{idea.title}</h1>
-  </div>
+  <Grid columns={2}>
+    <Grid.Row>
+      <Grid.Column width="10">
+        <Item>
+          <Item.Content>
+            <Item.Header as="h1">{idea.title}</Item.Header>
+            <Item.Meta>
+              <Label color="blue">{idea.category.name}</Label>
+            </Item.Meta>
+            <Divider />
+            <Item.Description>{idea.description}</Item.Description>
+            <Divider />
+            <Item.Extra>
+              <Label>GONNA HAVE KEYWORD LATER</Label>
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </Grid.Column>
+      <Grid.Column width="6">
+        <CommentsGroup comments={idea.comments} />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 );
 
 export default IdeaPage;
