8d8502b751f0aff1df62d05196a16a6ab1331745
EQuimper <quimperemanuel@gmail.com>
2016-12-21 05:40:30 -0500

    Make new reducer for length reactive

--- giveMeThatIdea_client/src/modules/ui/IdeasFollowReducer.js
+++ giveMeThatIdea_client/src/modules/ui/IdeasFollowReducer.js
@@ -1,12 +1,47 @@
-import { FETCH_USERS_FOLLOW_IDEA, FOLLOW_IDEA } from '../../modules';
+import { combineReducers } from 'redux';
+import { FETCH_USERS_FOLLOW_IDEA, FOLLOW_IDEA, UNFOLLOW_IDEA, FETCH_LENGTH_FOLLOW } from '../../modules';
 
-export default (state = [], action) => {
+const follow = (state = [], action) => {
   switch (action.type) {
     case FETCH_USERS_FOLLOW_IDEA:
       return action.ideasFollow;
     case FOLLOW_IDEA:
-      return [...state, action.id]
+      return [...state, action.id];
+    case UNFOLLOW_IDEA:
+      return state.filter(item => item !== action.id);
     default:
       return state;
   }
 };
+
+const fetchLengthReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_LENGTH_FOLLOW:
+      return action.lengthFollow;
+    case FOLLOW_IDEA:
+      return [...state.map(idea =>
+        idea.id === action.id ?
+        {
+          ...idea,
+          followers: idea.followers + 1
+        } :
+        idea
+      )];
+    case UNFOLLOW_IDEA:
+      return [...state.map(idea =>
+        idea.id === action.id ?
+        {
+          ...idea,
+          followers: idea.followers - 1
+        } :
+        idea
+      )];
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  ideas: follow,
+  lengthFollow: fetchLengthReducer
+});
