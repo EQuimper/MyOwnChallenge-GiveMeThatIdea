d9a6d4511bfd8c60aa67f8101d25614eb45c22f4
EQuimper <quimperemanuel@gmail.com>
2016-12-21 03:53:45 -0500

    Add follow function

--- giveMeThatIdea_client/src/modules/idea/feed/FeedIdeas.js
+++ giveMeThatIdea_client/src/modules/idea/feed/FeedIdeas.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Grid, Card, Container, Button, Icon, Segment, Label } from 'semantic-ui-react';
+import { Grid, Card, Button, Icon, Label } from 'semantic-ui-react';
 import { browserHistory } from 'react-router';
 import { truncateString } from '../../../helpers/truncateString';
 import { LikesGroup, FollowCommentsMeta } from './components';
 
-const FeedIdeas = ({ ideas }) => (
+const checkIfFollow = (arr, id) => arr.includes(id);
+
+const changeColor = name => {
+  switch (name) {
+    case 'Idea for web developer':
+      return 'teal';
+    case 'Idea for web developer portfolio':
+      return 'red';
+    default:
+      return null;
+  }
+};
+
+const FeedIdeas = ({ ideas, ideasFollow, followIdea }) => (
   <Grid centered columns={1}>
     <Grid.Column>
       <Card.Group>
         {ideas.map((idea, i) => (
           <Card key={i}>
-            <Label attached="top">
+            <Label attached="top" color={changeColor(idea.category.name)}>
               <Icon name="idea" color="black" disabled size="large" />
               {idea.category.name}
             </Label>
@@ -21,7 +34,10 @@ const FeedIdeas = ({ ideas }) => (
               </Card.Header>
               <br />
               <Card.Meta>
-                <FollowCommentsMeta followers={2} comments={idea.comments.length} />
+                <FollowCommentsMeta
+                  followers={idea.usersFollow.length}
+                  comments={idea.comments.length}
+                />
               </Card.Meta>
               <br />
               <Card.Description>
@@ -33,8 +49,14 @@ const FeedIdeas = ({ ideas }) => (
               <LikesGroup liked={10} disliked={5} />
             </Card.Content>
             <Card.Content extra>
-              <div className='ui two buttons'>
-                <Button basic color="blue">Follow</Button>
+              <div className="ui two buttons">
+                <Button
+                  basic={!checkIfFollow(ideasFollow, idea._id)}
+                  color="blue"
+                  onClick={() => followIdea(idea._id)}
+                >
+                  Follow
+                </Button>
                 <Button
                   basic
                   color="black"
