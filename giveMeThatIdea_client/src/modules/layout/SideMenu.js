40acf0c97752875f834655082421841cf150872f
EQuimper <quimperemanuel@gmail.com>
2016-12-20 21:48:22 -0500

    New design

--- giveMeThatIdea_client/src/modules/layout/SideMenu.js
+++ giveMeThatIdea_client/src/modules/layout/SideMenu.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Dropdown, Icon, Input, Menu } from 'semantic-ui-react';
-import { browserHistory } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 
-const SideMenu = () => (
+const SideMenu = ({ path }) => (
   <Menu vertical>
-    <Menu.Item>
+    <Menu.Item active={path === '/ideas'} onClick={() => browserHistory.push('/ideas')}>
       Ideas
-      <Menu.Menu>
-        <Menu.Item>
-          My ideas
-        </Menu.Item>
-        <Menu.Item onClick={() => browserHistory.push('/ideas/new')}>
-          Add One
-        </Menu.Item>
-        <Menu.Item>
-          Follow
-        </Menu.Item>
-      </Menu.Menu>
+      <Icon name="block layout" size="large" />
+    </Menu.Item>
+    <Menu.Item>
+      My ideas
+      <Icon name="idea" size="large" />
+    </Menu.Item>
+    <Menu.Item active={path === '/ideas/new'} onClick={() => browserHistory.push('/ideas/new')}>
+      Add One
+      <Icon name="add circle" size="large" />
+    </Menu.Item>
+    <Menu.Item>
+      Follow
+      <Icon name="star" size="large" />
     </Menu.Item>
   </Menu>
 );
