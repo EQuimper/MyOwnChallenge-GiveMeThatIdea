69d69c9cb720694f16c5d03f8ada49520e952bfa
EQuimper <quimperemanuel@gmail.com>
2016-12-21 03:50:52 -0500

    Routes for new comment

--- giveMeThatIdea_server/server/modules/comment/routes.js
+++ giveMeThatIdea_server/server/modules/comment/routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import * as CommentController from './controller';
+import { requireAuth } from '../../helpers';
 
 const routes = new Router();
 
-routes.route('/ideas/:ideaId/comments/new').post(CommentController.createComment);
+routes.route('/ideas/:id/comments/new').post(requireAuth, CommentController.createComment);
 
 export default routes;
