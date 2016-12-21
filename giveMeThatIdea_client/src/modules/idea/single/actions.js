405ac655887d4943de723e0c1d249ba42b929c2e
EQuimper <quimperemanuel@gmail.com>
2016-12-20 21:48:17 -0500

    create comment

--- giveMeThatIdea_client/src/modules/idea/single/actions.js
+++ giveMeThatIdea_client/src/modules/idea/single/actions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_IDEA = 'FETCH_IDEA';
+export const CREATE_COMMENT = 'CREATE_COMMENT';
 
 export const fetchIdea = slug => {
   const promise = new Promise((resolve, reject) => {
@@ -16,3 +17,9 @@ export const fetchIdea = slug => {
     payload: promise
   };
 }
+
+export const createComment = values => (dispatch, getState) => {
+  const userId = getState().auth.user.id;
+  const ideaId = getState().api.idea.idea._id;
+  const { text } = values;
+};
