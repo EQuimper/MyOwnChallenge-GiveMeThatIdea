9c4e97112a351948f6461dc8789ae1db4282faa5
EQuimper <quimperemanuel@gmail.com>
2016-12-21 03:51:11 -0500

    Can now follow idea

--- giveMeThatIdea_server/server/modules/idea/routes.js
+++ giveMeThatIdea_server/server/modules/idea/routes.js
@@ -7,8 +7,9 @@ const routes = new Router();
 routes.route('/ideas/new').post(requireAuth, IdeaController.createIdea);
 routes.route('/ideas/:id').delete(requireAuth, IdeaController.deleteIdea);
 routes.route('/ideas/:id').put(requireAuth, IdeaController.updateIdea);
-routes.route('/ideas/').get(requireAuth, IdeaController.getAllIdea);
+routes.route('/ideas').post(requireAuth, IdeaController.getAllIdea);
 routes.route('/ideas/:slug').get(requireAuth, IdeaController.getOneIdea);
 routes.route('/ideas/asyncIdeaTitle').post(requireAuth, IdeaController.asyncIdeaTitle);
+routes.route('/ideas/:id/followIdea').post(requireAuth, IdeaController.followIdea);
 
 export default routes;
