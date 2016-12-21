3431c536e2de9cec2cd947c03a8227b8204b9119
EQuimper <quimperemanuel@gmail.com>
2016-12-21 03:51:31 -0500

    add usersfollow in model

--- giveMeThatIdea_server/server/modules/idea/model.js
+++ giveMeThatIdea_server/server/modules/idea/model.js
@@ -30,6 +30,7 @@ const IdeaSchema = new Schema({
     unique: true,
     lowercase: true
   },
+  usersFollow: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 }, { timestamps: true });
 
