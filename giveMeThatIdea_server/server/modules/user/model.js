7009953582318d111782f1894eb58f4882f7c499
EQuimper <quimperemanuel@gmail.com>
2016-12-28 21:03:02 -0500

    Better use of bcypt

--- giveMeThatIdea_server/server/modules/user/model.js
+++ giveMeThatIdea_server/server/modules/user/model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
-import bcrypt from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 import { isEmail } from 'validator';
 
 const UserSchema = new Schema({
@@ -55,10 +55,10 @@ UserSchema.pre('save', function(next) {
   // number of round for salt
   const SALT_ROUNDS = 10;
   // hash the password
-  bcrypt.hash(this.local.password, SALT_ROUNDS, (err, hash) => {
+  hash(this.local.password, SALT_ROUNDS, (err, hashPasword) => {
     if (err) { return next(err); }
     // make the local.password the result of the hash
-    this.local.password = hash;
+    this.local.password = hashPasword;
     next();
   });
 });
@@ -66,7 +66,7 @@ UserSchema.pre('save', function(next) {
 // method for comapre password
 UserSchema.methods.comparePassword = function(canditePassword, cb) {
   // compare the password from the front-end and check if match the crypt one
-  bcrypt.compare(canditePassword, this.local.password, (err, isMatch) => {
+  compare(canditePassword, this.local.password, (err, isMatch) => {
     if (err) { return cb(err); }
     // return no error and the match if this is good
     cb(null, isMatch);
